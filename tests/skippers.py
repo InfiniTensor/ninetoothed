419d1949c1b4f65c1af469444cb239dc20c027f0
Jiacheng Huang <huangjiacheng0709@outlook.com>
2024-08-02 16:22:26 +0800

    Fix unexpected start of the CUDA tests

--- tests/skippers.py
+++ tests/skippers.py
@@ -4,7 +4,7 @@ import torch
 
 def skip_if_cuda_not_available(func):
     return pytest.mark.skipif(
-        not torch.cuda.is_available,
+        not torch.cuda.is_available(),
         reason="CUDA not available",
     )(func)
 
