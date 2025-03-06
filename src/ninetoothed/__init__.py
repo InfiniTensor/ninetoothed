831d752e03b81a681420c0607f0ec78ce548e05c
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-03-06 17:06:07 +0800

    Add basic data types

--- src/ninetoothed/__init__.py
+++ src/ninetoothed/__init__.py
@@ -1,6 +1,35 @@
+from ninetoothed.dtype import (
+    float16,
+    float32,
+    float64,
+    int8,
+    int16,
+    int32,
+    int64,
+    uint8,
+    uint16,
+    uint32,
+    uint64,
+)
 from ninetoothed.jit import jit
 from ninetoothed.make import make
 from ninetoothed.symbol import Symbol
 from ninetoothed.tensor import Tensor
 
-__all__ = ["Symbol", "Tensor", "jit", "make"]
+__all__ = [
+    "Symbol",
+    "Tensor",
+    "float16",
+    "float32",
+    "float64",
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "jit",
+    "make",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+]
---
+++ src/ninetoothed/dtype.py
@@ -0,0 +1,13 @@
+int8 = "i8"
+int16 = "i16"
+int32 = "i32"
+int64 = "i64"
+
+uint8 = "u8"
+uint16 = "u16"
+uint32 = "u32"
+uint64 = "u64"
+
+float16 = "fp16"
+float32 = "fp32"
+float64 = "fp64"
