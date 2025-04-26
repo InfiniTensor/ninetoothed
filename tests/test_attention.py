18e4315ce89fb2e9ea42ec18b64e807a9a8fb170
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-04-26 15:43:11 +0800

    Use `dtype` access instead of hardcoding

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -34,7 +34,7 @@ def arrangement(q, k, v, o):
 
 
 def application(q, k, v, o):
-    q_loaded = (q * 1.44269504089).to(ntl.float16)
+    q_loaded = (q * 1.44269504089).to(q.dtype)
 
     acc = ntl.zeros((q.shape[-2], q.shape[-1]), dtype=ntl.float32)
     l_i = ntl.full((q.shape[-2],), 1, dtype=ntl.float32)
@@ -48,7 +48,7 @@ def application(q, k, v, o):
         l_ij = ntl.sum(p, 1)
 
         alpha = ntl.exp2(m_i - m_ij)
-        acc = acc * alpha[:, None] + ntl.dot(p.to(ntl.float16), v[i])
+        acc = acc * alpha[:, None] + ntl.dot(p.to(v[i].dtype), v[i])
         m_i = m_ij
         l_i = l_i * alpha + l_ij
 
