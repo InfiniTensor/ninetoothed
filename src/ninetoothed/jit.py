1279f84fa9f00fef9ddcbd510b19f5bea724ec43
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-07-12 21:47:08 +0800

    Move the `_import_from_path` method from the `JIT` class to a function `import_from_path` outside the class

--- src/ninetoothed/jit.py
+++ src/ninetoothed/jit.py
@@ -97,7 +97,7 @@ class JIT:
             self._max_num_configs,
             self._prettify,
         )
-        module = type(self)._import_from_path(source_file, source_file)
+        module = import_from_path(source_file, source_file)
         module_vars = vars(module)
 
         handle = _Handle(
@@ -108,14 +108,14 @@ class JIT:
 
         return handle
 
-    @staticmethod
-    def _import_from_path(module_name, file_path):
-        spec = importlib.util.spec_from_file_location(module_name, file_path)
-        module = importlib.util.module_from_spec(spec)
-        sys.modules[module_name] = module
-        spec.loader.exec_module(module)
 
-        return module
+def import_from_path(module_name, file_path):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    return module
 
 
 class _Handle:
