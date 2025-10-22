0aca7ba6adb2466f38543d9a11fd14ebb8035d53
Jiacheng Huang <huangjiacheng0709@outlook.com>
2025-10-22 10:25:47 +0800

    Prevent partial word replacements in `Tensor.eval` by using regex word boundaries

--- src/ninetoothed/eval.py
+++ src/ninetoothed/eval.py
@@ -1,5 +1,6 @@
 import copy
 import math
+import re
 
 import numpy as np
 
@@ -52,7 +53,7 @@ def _eval(tensor, subs=None):
         for old, new in sorted(
             replacements.items(), key=lambda key: len(key), reverse=True
         ):
-            string = string.replace(old, new)
+            string = re.sub(rf"\b{re.escape(old)}\b", new, string)
 
         return string
 
