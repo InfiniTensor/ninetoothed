b2760f831b728dea61594dd3a4604ae43781d759
zhangxin <xz89166@gmail.com>
2026-01-06 13:33:38 +0000

    add a demo test for assigning values to sliced tensor

---
+++ tests/test_slice_assignment.py
@@ -0,0 +1,23 @@
+import ninetoothed as nt
+import torch
+
+def assign(lhs, rhs, BLOCK_SIZE):
+
+    @nt.jit
+    def add_kernel(
+        lhs: nt.Tensor(1)[:BLOCK_SIZE],
+        rhs: nt.Tensor(1)[:BLOCK_SIZE],
+    ):
+        lhs = rhs
+
+    add_kernel(lhs, rhs)
+
+if __name__ == "__main__":
+    DEV = "cuda"
+    x = torch.randn(20, device=DEV)
+    y = torch.zeros_like(x)
+    print("before assign")
+    print(y)
+    assign(y, x, BLOCK_SIZE=10)
+    print("after assign")
+    print(y)
