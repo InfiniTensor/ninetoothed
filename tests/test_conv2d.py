47812f21d3f65270e3280a734a12f51e6f32ef72
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-01-15 03:20:21 +0000

    Integrate padding testing into `test_conv2d.py` and remove `test_conv2d_pad.py`

--- tests/test_conv2d.py
+++ tests/test_conv2d.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 import ninetoothed
 import tests.test_matmul as matmul
-from ninetoothed import Tensor
+from ninetoothed import Symbol, Tensor
 from tests.utils import get_available_devices
 
 
@@ -18,7 +18,11 @@ def arrangement(
     BLOCK_SIZE_N=matmul.BLOCK_SIZE_N,
     BLOCK_SIZE_K=matmul.BLOCK_SIZE_K,
 ):
-    input_tiled = input.tile((1, *filter.shape[1:]), strides=(-1, -1, 1, 1))
+    pad_h = Symbol("pad_h", constexpr=True)
+    pad_w = Symbol("pad_w", constexpr=True)
+
+    input_padded = input.pad((pad_w, pad_w, pad_h, pad_h))
+    input_tiled = input_padded.tile((1, *filter.shape[1:]), strides=(-1, -1, 1, 1))
     input_squeezed = input_tiled.squeeze(1)
     input_squeezed.dtype = input_squeezed.dtype.squeeze(0)
     input_raveled = input_squeezed.ravel()
@@ -37,11 +41,16 @@ def arrangement(
     )(input_flattened, filter_permuted, output_flattened)
 
 
-def conv2d(input, filter):
+def conv2d(input, filter, padding=0):
+    if isinstance(padding, int):
+        padding = (padding, padding)
+
+    pad_h, pad_w = padding
+
     n, _, h, w = input.shape
     k, _, r, s = filter.shape
-    p = h - r + 1
-    q = w - s + 1
+    p = h + 2 * pad_h - r + 1
+    q = w + 2 * pad_w - s + 1
 
     output = torch.empty((n, k, p, q), device=input.device, dtype=input.dtype)
 
@@ -52,13 +61,14 @@ def conv2d(input, filter):
         max_num_configs=50,
     )
 
-    conv2d_kernel(input, filter, output)
+    conv2d_kernel(input, filter, output, pad_h=pad_h, pad_w=pad_w)
 
     return output
 
 
 @pytest.mark.parametrize("device", get_available_devices())
 @pytest.mark.parametrize("dtype, rtol, atol", ((torch.float16, 0.001, 0.001),))
+@pytest.mark.parametrize("padding", (0, (1, 1), (0, 1), (2, 0)))
 @pytest.mark.parametrize("s", (3,))
 @pytest.mark.parametrize("r", (3,))
 @pytest.mark.parametrize("k", (512,))
@@ -66,11 +76,11 @@ def conv2d(input, filter):
 @pytest.mark.parametrize("h", (16,))
 @pytest.mark.parametrize("c", (64,))
 @pytest.mark.parametrize("n", (4,))
-def test(n, c, h, w, k, r, s, dtype, device, rtol, atol):
+def test(n, c, h, w, k, r, s, padding, dtype, device, rtol, atol):
     input = torch.rand((n, c, h, w), dtype=dtype, device=device)
     weight = torch.rand((k, c, r, s), dtype=dtype, device=device)
 
-    output = conv2d(input, weight)
-    expected = F.conv2d(input, weight)
+    output = conv2d(input, weight, padding=padding)
+    expected = F.conv2d(input, weight, padding=padding)
 
     assert torch.allclose(output, expected, rtol=rtol, atol=atol)
