f4598e4d5f5350e5189faefa47359393fec1acb2
Jiacheng Huang <huangjiacheng0709@outlook.com>
2026-01-27 10:25:26 +0800

    Rename `nt_tuner.py` to `auto_tuner.py`

--- src/ninetoothed/__init__.py
+++ src/ninetoothed/__init__.py
@@ -1,3 +1,4 @@
+from ninetoothed.auto_tuner import NtTuner
 from ninetoothed.build import build
 from ninetoothed.dtype import (
     bfloat16,
@@ -17,7 +18,6 @@ from ninetoothed.eval import _eval as eval
 from ninetoothed.eval import _subs as subs
 from ninetoothed.jit import jit
 from ninetoothed.make import make
-from ninetoothed.nt_tuner import NtTuner
 from ninetoothed.symbol import Symbol, block_size
 from ninetoothed.tensor import Tensor
 
